e55123f0beca99cfa431a8a1ad880da96a38186e
Johann Le Biannic <jlebiannic@sodifrance.fr>
2023-11-22 11:59:56 +0100

    part1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
+<script lang="ts" setup>
+import datas from "@/datas/db";
+import Resource from "@/interfaces/resourceInterface";
+const resources: Resource[] = datas;
+</script>
+
 <template>
   <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-title>DevWall</v-app-bar-title>
+      <v-btn>Menu1</v-btn>
+      <v-btn>Menu2</v-btn>
+    </v-app-bar>
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <!-- <ul>
+            <li v-for="resource in resources">{{ resource.title }}</li>
+          </ul> -->
+        <v-card v-for="resource in resources">
+          <!-- <img :src="resource.image" height="250" /> -->
+          <v-img :src="resource.image" height="250"></v-img>
+          <v-card-title>{{ resource.title }}</v-card-title>
+          <v-card-text
+            >{{ resource.media }} ajouté le {{ resource.date }}</v-card-text
+          >
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
-
-<script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
---
+++ src/interfaces/resourceInterface.ts
@@ -0,0 +1,18 @@
+type Lang = "fr" | "en";
+type Media = "post" | "video" | "book";
+
+interface Resource {
+  id: string;
+  media: Media;
+  title: string;
+  lang: Lang;
+  image: string;
+  url: string;
+  description: string;
+  rating: number;
+  isTop: boolean;
+  isValid: boolean;
+  date: string;
+}
+
+export default Resource;
